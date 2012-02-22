e7a0ac210ff4010c7ee8d743e92a0fa9325b1619
hisasann <hisasann25@gmail.com>
2012-02-22 14:59:51 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function() {
+    location.href = "https://api.instagram.com/oauth/authorize/?client_id=a09cc5c96225438dbbe830c0dac2ab69&redirect_uri=http://hisasann.github.com/Ramenstagram/&response_type=code";
+
+    $("#refresh").click(function() {
+
+    });
+});
