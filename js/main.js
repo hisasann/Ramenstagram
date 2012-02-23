b00f78c2518416220153595be1e15c7fa5d682a2
hisasann <hisasann25@gmail.com>
2012-02-23 19:42:08 +0900

    OAuthに対応してみた

--- js/main.js
+++ js/main.js
@@ -4,12 +4,25 @@ $(function () {
         refresh = $("#refresh"),
         loading = $("#loading");
 
-    refresh
-        .bind("click", function() {
-            get_instagram_image();
-        })
-        .trigger("click");
+    if (is_has_access_token()) {
+        event_setter();
+    } else {
+        var code = get_has_code();
+        if (code) {
+            save_access_token(code);
+            event_setter();
+        } else {
+            show_oauth_authorize();
+        }
+    }
 
+    function event_setter() {
+        refresh
+            .bind("click", function() {
+                get_instagram_image();
+            })
+            .trigger("click");
+    }
 
     function get_instagram_image() {
         loading.show();
@@ -20,7 +33,8 @@ $(function () {
             url:"https://api.instagram.com/v1/tags/" + TAG_NAME + "/media/recent",
             type:"GET",
             data:{
-                "access_token":"197750.f59def8.d84e874c9cf74779a2df73cc4e53056f"
+//                "access_token":"197750.f59def8.d84e874c9cf74779a2df73cc4e53056f"
+                "access_token":$.cookie("access_token")
             },
             cache:false,
             dataType:"jsonp"
@@ -59,60 +73,48 @@ $(function () {
         return list;
     }
 
-//    var code = is_has_code();
-//    if (code) {
-//        save_access_token(code);
-//    } else {
-//        if (!code) {
-//            show_oauth_authorize();
-//        }
-//    }
-//
-//    $("#refresh").click(function() {
-//
-//    });
-//
-//    function is_has_code() {
-//        var query = window.location.search.substring(1);
-//        var param = query.split("=");
-//
-//        if (param[0] !== "code") {
-//            return null;
-//        }
-//
-//        return param[1];
-//    }
-//
-//    function is_has_access_token() {
-//        return !!$.cookie("access_token");
-//    }
-//
-//    function save_access_token(code) {
-//        var xhr = $.ajax({
-//            url: "https://api.instagram.com/oauth/access_token",
-//            type: "POST",
-//            data: {
-//                "client_id": "a09cc5c96225438dbbe830c0dac2ab69",
-//                "client_secret": "ada9cc93f616443c8aaab3f8779309c0",
-//                "grant_type": "authorization_code",
-//                "redirect_uri": "http://hisasann.github.com/Ramenstagram/",
-//                "code": code
-//            },
-//            cache: false,
-//            dataType: "json"
-//        });
-//
-//        xhr.done(function(json) {
-//            console.log(json);
-//            $.cookie("access_token", json.access_token, { path: "/", expires: 365 });
-//        });
-//
-//        xhr.fail(function(jqXHR, textStatus) {
-//            alert( "Request failed: " + textStatus );
-//        });
-//    }
-//
-//    function show_oauth_authorize() {
-//        location.href = "https://api.instagram.com/oauth/authorize/?client_id=a09cc5c96225438dbbe830c0dac2ab69&redirect_uri=http://hisasann.github.com/Ramenstagram/&response_type=code";
-//    }
+    function get_has_code() {
+        var query = window.location.search.substring(1);
+        var param = query.split("=");
+
+        if (param[0] !== "code") {
+            return null;
+        }
+
+        return param[1];
+    }
+
+    function is_has_access_token() {
+        return !!$.cookie("access_token");
+    }
+
+    function save_access_token(code) {
+        var xhr = $.ajax({
+            url: "https://api.instagram.com/oauth/access_token",
+            type: "POST",
+            data: {
+                "client_id": "a09cc5c96225438dbbe830c0dac2ab69",
+                "client_secret": "ada9cc93f616443c8aaab3f8779309c0",
+                "grant_type": "authorization_code",
+                "redirect_uri": "http://hisasann.github.com/Ramenstagram/",
+                "code": code
+            },
+            cache: false,
+            dataType: "jsonp"
+        });
+
+        xhr.done(function(json) {
+            console.log(json);
+            $.cookie("access_token", json.access_token, { path: "/", expires: 365 });
+        });
+
+        xhr.fail(function(jqXHR, textStatus) {
+            alert( "Request failed: " + textStatus );
+        });
+    }
+
+    function show_oauth_authorize() {
+        location.href = "https://api.instagram.com/oauth/authorize/?client_id=a09cc5c96225438dbbe830c0dac2ab69&redirect_uri=http://hisasann.github.com/Ramenstagram/&response_type=code";
+    }
+
 });
