7d390450bb97003c41eee0df14b61d838716a8a8
hisasann <hisasann25@gmail.com>
2012-02-23 19:34:24 +0900

    loading出すときはデータ消したいよね

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ $(function () {
 
     function get_instagram_image() {
         loading.show();
+        elem.html("");
 
         var TAG_NAME = "ramen";
         var xhr = $.ajax({
@@ -37,8 +38,6 @@ $(function () {
     }
 
     function show_instagram_image(json) {
-        elem.html("");
-
         var html = [];
         $.each(json, function (index, data) {
             html.push('<a href="' + data.link + '" target="_blank"><img src="' + data.images.low_resolution.url + '"></a>');
